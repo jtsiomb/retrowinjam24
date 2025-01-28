622497802edbe18dae6eba29389b3b3b2a318d26
John Tsiombikas <nuclear@member.fsf.org>
2025-01-28 21:25:42 +0200

    added imgquant as a submodule

--- tools/data.mk
+++ tools/data.mk
@@ -1,9 +1,18 @@
 .PHONY: all
 all: data/tileset.png data/test.fnt
 
-data/tileset.png: datasrc/rendscript datasrc/dungeon.obj tools/tilerend/tilerend
-	tools/tilerend/tilerend -o render.png $<
-	imgquant render.png -P -o $@ -C 254 -s 16 -os data/tileset.slut
+TILEREND = tools/tilerend/tilerend
+IMGQUANT = tools/imgquant/imgquant
+
+data/tileset.png: datasrc/rendscript datasrc/dungeon.obj $(TILEREND) $(IMGQUANT)
+	$(TILEREND) -o render.png $<
+	$(IMGQUANT) render.png -P -o $@ -C 254 -s 16 -os data/tileset.slut
 
 data/test.fnt: /usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf
 	font2glyphmap -size 16 -range 32-126 -padding 0 -o $@ $<
+
+$(TILEREND):
+	cd tools/tilerend && $(MAKE)
+
+$(IMGQUANT):
+	cd tools/imgquant && $(MAKE)
