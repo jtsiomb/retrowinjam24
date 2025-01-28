2fa791bb75b94760b43000b2ab20a797699d7a2d
John Tsiombikas <nuclear@member.fsf.org>
2025-01-28 05:31:13 +0200

    assetpipe: pass tilerend output through imgquant

--- tools/data.mk
+++ tools/data.mk
@@ -2,7 +2,8 @@
 all: data/tileset.png data/test.fnt
 
 data/tileset.png: datasrc/rendscript datasrc/dungeon.obj tools/tilerend/tilerend
-	tools/tilerend/tilerend -o $@ $<
+	tools/tilerend/tilerend -o render.png $<
+	imgquant render.png -P -o $@ -C 254 -s 16 -os data/tileset.slut
 
 data/test.fnt: /usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf
 	font2glyphmap -size 16 -range 32-126 -padding 0 -o $@ $<
